bb7acd81643abe674f332d9527dfedfbfdb9ef72
goldyfruit <gaetan.trellu@ibm.com>
2021-02-26 12:31:35 -0500

    Small refact

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
+[![Build Status](https://travis-ci.com/smartgic/mycroft-sonos-controller-skill.svg?branch=20.8.1)](https://travis-ci.com/github/smartgic/mycroft-sonos-controller-skill) [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) [![contributions welcome](https://img.shields.io/badge/contributions-welcome-pink.svg?style=flat)](https://github.com/smartgic/mycroft-sonos-controller-skill/pulls) [![Skill: MIT](https://img.shields.io/badge/mycroft.ai-skill-blue)](https://mycroft.ai) [![Discord](https://img.shields.io/discord/809074036733902888)](https://discord.gg/Vu7Wmd9j) 
+
 # <img src='https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/flag-checkered.svg' card_color='#40DBB0' width='50' height='50' style='vertical-align:bottom'/> Finished Booting
-Skill to determine when Mycroft AI has finished booting up.
+
+Play a sound or speak when Mycroft AI has finished booting up
 
 ## About 
 
-With this skill Mycroft will say "finished booting" or play a startup sound when the boot up period has finished and is ready to receive commands. 
+With this skill Mycroft will say "finished booting" or play a sound when the boot up period has finished and is ready to receive commands.
 
 ## Examples 
 
 ## Credits 
-zelmon64 (@zelmon64), Wally Fort (@fortwally), JarbasAI (@JarbasAI), Luke (@luke5sky), benklop (@benklop)
+zelmon64 (@zelmon64)
+Wally Fort (@fortwally)
+JarbasAI (@JarbasAI)
+Luke (@luke5sky)
+benklop (@benklop)
+goldyfruit (@goldyfruit)
 
 ## Category
+
 **Configuration**
 
 You may optionally add a configuration stanza to mycroft.conf to play an mp3 file instead of speaking:
--- __init__.py
+++ __init__.py
@@ -1,49 +1,57 @@
-#~ skill-finished-booting - A simple skill to know when mycroft finishes booting up.
-#~ Copyright (C) 2018  zelmon64
-#~ Github - https://github.com/zelmon64
+# Copyright (C) 2018 zelmon64
+# Github - https://github.com/zelmon64
 
-#~ This program is free software: you can redistribute it and/or modify
-#~ it under the terms of the GNU General Public License as published by
-#~ the Free Software Foundation, either version 3 of the License, or
-#~ (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-#~ This program is distributed in the hope that it will be useful,
-#~ but WITHOUT ANY WARRANTY; without even the implied warranty of
-#~ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#~ GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-#~ You should have received a copy of the GNU General Public License
-#~ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from mycroft import MycroftSkill
-from mycroft.util.log import LOG
-from mycroft.configuration import ConfigurationManager
 from mycroft.util import play_mp3
+import os
+
 
 class FinishedBootingSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
-        super(FinishedBootingSkill, self).__init__(name="FinishedBootingSkill")
+        MycroftSkill.__init__(self)
+        self.mp3_file = None
 
     def initialize(self):
+        # Callback when setting changes are detected from home.mycroft.ai
+        self.settings_change_callback = self.on_websettings_changed
+        self.on_websettings_changed()
+
+    def on_websettings_changed(self):
+        self._run()
+
+    def _run(self):
+        self.mp3_file = self.settings.get('mp3_file')
         self.add_event("mycroft.skills.initialized", self.handle_boot_finished)
-        LOG.debug('add event handle boot finished')
-        config = ConfigurationManager.get()
-        base_conf = config.get('FinishedBootingSkill')
-        if base_conf:
-          self.mp3_file = base_conf.get('startup_mp3', None)
-        else:
-          self.mp3_file = None
+        self.log.debug('add event handle boot finished')
+        self.log.debug('mp3 file used: {}'.format(self.mp3_file))
 
     def handle_boot_finished(self):
-        if self.mp3_file:
-            play_mp3(self.mp3_file)
-        else:
-            self.speak_dialog('finished.booting')
-        LOG.debug('finished booting')
+        try:
+            if self.mp3_file:
+                if os.path.isfile(self.mp3_file):
+                    play_mp3(self.mp3_file)
+            else:
+                self.speak_dialog('finished.booting')
+            self.log.debug('finished booting')
+        except Exception as e:
+            self.log.error('unable to handle boot finished notification')
+            self.log.debug(e)
 
-# The "create_skill()" method is used to create an instance of the skill.
-# Note that it's outside the class itself.
+
 def create_skill():
     return FinishedBootingSkill()
